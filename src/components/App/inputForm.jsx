f8a45e8013e015fb112ab5d8b0f70fec8ee49bf7
Max Lamb <maxlambcodes@gmail.com>
2020-07-21 13:50:10 -0700

    form created

---
+++ src/components/App/inputForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const inputForm = ({ username, onChange, onSubmit }) => {
+  <form onSubmit={onSubmit}>
+    <input type='text' value={username} onChange={onChange}/>
+    <button>Submit</button>
+  </form>;
+};
+
+inputForm.propTypes = {
+  username: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  onSubmit: propTypes.func.isRequired
+};
+
+export default inputForm;
