50b25c87bb1272048f533d10d2ca213a1bd84970
Max Lamb <maxlambcodes@gmail.com>
2020-07-21 17:52:43 -0700

    thank you for helping me pass this test

--- src/components/App/Display.test.jsx
+++ src/components/App/Display.test.jsx
@@ -4,7 +4,7 @@ import Display from './Display';
 
 describe('Display component', () => {
   it('renders User Display', () => {
-    const wrapper = shallow(<Display username="crazyTony" followers={300} following={200} html_url={'http://website.gov'} repos={['stuff']}/>);
+    const wrapper = shallow(<Display username="crazyTony" followers={300} following={200} html_url={'http://website.gov'} repos={[{ name: 'crazyTony', html_url: 'http://website.gov' }]}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
